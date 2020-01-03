e0ef2e678cc014173ed6efb49b7c3ea483375dce
lucho1503 <lmmc.1503@gmail.com>
2020-01-03 02:49:52 +0000

    setup_mysql_dev.sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- this script prepare a new server MYSQL
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db
+       CHARSET utf8mb4
+       COLLATE utf8mb4_unicode_ci;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
