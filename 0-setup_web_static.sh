b32a1969542b426f4e06d2253c5c7de2790e0d9b
lucho1503 <lmmc.1503@gmail.com>
2020-01-12 20:17:43 +0000

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,6 @@ mkdir -p /data/web_static/shared/
 echo "Holbertons School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-sudo sed -i "38i\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/\
-;\n\t\tautoindex off ; \n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-enabled/default
 sudo service nginx restart
 exit 0
