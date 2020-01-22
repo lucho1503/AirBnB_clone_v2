526a604faabd0feddf662181d8a88337d9a86fb5
lucho1503 <lmmc.1503@gmail.com>
2020-01-22 22:27:50 +0000

    pep8

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ starts a flask application """
 
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def hello_HBNB():
     """ print hello HBNB """
     return "Hello HBNB!"
