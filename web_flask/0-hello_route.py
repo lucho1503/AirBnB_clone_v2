ab66e2b7099b02357794271bb809ace1473efbb8
lucho1503 <lmmc.1503@gmail.com>
2020-01-22 22:15:15 +0000

    0-hello_route.py

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+
+def hello_HBNB():
+    return "Hello HBNB!"
+
+app.run(host='0.0.0.0', port=5000)
