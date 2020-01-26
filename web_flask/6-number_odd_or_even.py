f68189d07a9d83c0538bef152b1dba66bd2a31d3
lucho1503 <lmmc.1503@gmail.com>
2020-01-26 18:43:51 +0000

    6-number_odd_or_even.py

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" this script start a new application flask """
+
+
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """ print Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ print HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    return "C " + text.replace("_", " ")
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<py>', strict_slashes=False)
+def python_is_cool(py="is_cool"):
+    return "Python " + py.replace("_", " ")
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_int(n):
+    if isinstance(n, int):
+        return str(n) + " is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def num_int(n):
+    return render_template('5-number.html', num=n)
+
+
+@app.route('/number_odd_or_even/<int:number>', strict_slashes=False)
+def int_num(number):
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
