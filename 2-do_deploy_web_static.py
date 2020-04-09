a53b65ae272fda1966f9f17e8d335a404984272f
lucho1503 <lmmc.1503@gmail.com>
2020-04-09 21:39:38 +0000

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,22 +4,27 @@
 from fabric.api import *
 import os
 
-env.user = 'ubuntu'
-env.hosts = ['35.243.129.221', '35.231.144.56']
+env.hosts = ['35.237.185.211', '54.160.144.29']
 
 
 def do_deploy(archive_path):
-    if os.path.exists(archive_path):
-        put(archive_path, '/tmp/')
+    if not os.path.exists(archive_path):
+        return False
+    try:
         file_path = archive_path.split("/")[-1]
-        path_f = '/data/web_static/releases/{}'.format(file_path.split(".")[0])
-        run('mkdir -p {}'.format(path_fi))
-        run('tar -xzf /tmp/{} -C {}'.format(file_path, path_fi))
-        run('rm /tmp/{}'.format(file_path))
-        run('mv {}/web_static/* {}'.format(path_fi, path_fi))
-        run('rm -rf {}/web_static'.format(path_fi))
-        run('rm -rf /data/web_static/current')
-        run('ls -s {} /data/web_static/current'.format(path_fi))
+        path_fi = '/data/web_static/releases'
+        put("{}".format(archive_path), '/tmp/{}'.format(file_path))
+        spl = file_path.split(".")
+        sudo('mkdir -p {}/{}/'.format(path_fi, spl[0]))
+        new_file = ".".join(spl)
+        sudo('tar -xzf /tmp/{} -C {}/{}/'.format(new_file, path_fi, spl[0]))
+        sudo('rm /tmp/{}'.format(file_path))
+        sudo('mv {}/{}/web_static/* {}/{}/'.format(
+            path_fi, spl[0], path_fi, spl[0]))
+        sudo('rm -rf {}/{}/web_static'.format(path_fi, spl[0]))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/{} /data/web_static/current'.format(path_fi, spl[0]))
         print('its is a deployment')
         return True
-    return False
+    except:
+        return False
