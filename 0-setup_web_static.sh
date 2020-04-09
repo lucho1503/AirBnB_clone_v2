bbf4e0031a5f57c7c9745abfb9d213915cd5d536
lucho1503 <lmmc.1503@gmail.com>
2020-04-09 19:31:32 +0000

    shellcheck

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,13 +4,12 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-echo "<html>" > /data/web_static/releases/test/index.html
-echo "<head>" >> /data/web_static/releases/test/index.html
-echo "</head>" >> /data/web_static/releases/test/index.html
-echo "<body>" >> /data/web_static/releases/test/index.html
-echo "Holberton School" >> /data/web_static/releases/test/index.html
-echo "</body>" >> /data/web_static/releases/test/index.html
-echo "</html>" >> /data/web_static/releases/test/index.html
+echo "<html></html>" > /data/web_static/releases/test/index.html
+#echo "<head>" >> /data/web_static/releases/test/index.html
+#echo "</head>" >> /data/web_static/releases/test/index.html
+#echo "<body>" >> /data/web_static/releases/test/index.html
+#echo "Holberton School" >> /data/web_static/releases/test/index.html
+#echo "</body>" >> /data/web_static/releases/test/index.html"</html>"
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sudo sed -i "38i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-enabled/default
