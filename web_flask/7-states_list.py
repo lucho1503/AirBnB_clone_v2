ed1ffdc7d0077828f9e5fbe6d24649622dedb573
lucho1503 <lmmc.1503@gmail.com>
2020-04-22 19:38:11 +0000

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# start a application web flask
+
+
+from models import storage
+from flask import render_template, Flask
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """ close session SQLalchemy """
+    storage.close()
+
+
+@app.route('/states_list/', strict_slashes=False)
+def states_list():
+    """ list states for id : name and display in html file"""
+    states = storage.all(State)
+    dictt = {value.id: value.name for value in states.values()}
+    return render_template('7-states_list.html', Table="States", my_dict=dictt)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
