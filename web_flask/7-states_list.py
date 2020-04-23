465b466779901a97e1c85159b9053f5f43c96293
lucho1503 <lmmc.1503@gmail.com>
2020-04-23 01:07:38 +0000

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -23,5 +23,5 @@ def states_list():
     return render_template('7-states_list.html', Table="States", my_dict=dictt)
 
 
-#if __name__ == "__main__":
-app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
