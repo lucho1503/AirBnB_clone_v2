467dc3824bb9b095758bbdc2ecfa8a21b8893649
lucho1503 <lmmc.1503@gmail.com>
2020-04-23 01:02:39 +0000

    setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -3,7 +3,10 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db
        CHARSET utf8mb4
        COLLATE utf8mb4_unicode_ci;
+
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+USE hbnb_dev_db;
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
