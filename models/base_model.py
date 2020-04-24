dadc921a1062d31255a4a6cff3fe620a4c2dc64b
lucho1503 <lmmc.1503@gmail.com>
2020-04-24 23:52:03 +0000

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
 """This is the base model class for AirBnB"""
-import os
+
 import uuid
 import models
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime
 
-STORAGE_TYPE = os.environ.get('HBNB_TYPE_STORAGE')
+
 """
 crate a instance of Base if storage type is a database if not
 database storage, use class BaseModel
 """
-if STORAGE_TYPE == 'db':
-    Base = declarative_base()
-else:
-    class Base:
-        pass
+
+Base = declarative_base()
+
 
 
 class BaseModel:
     """This class will defines all common attributes/methods
     for other classes
     """
-    if STORAGE_TYPE == 'db':
-        id = Column(String(60), nullable=False, primary_key=True)
-        created_at = Column(DateTime, nullable=False,
-                            default=datetime.utcnow())
-        updated_at = Column(DateTime, nullable=False,
-                            default=datetime.utcnow())
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
+    updated_at = Column(DateTime, nullable=False, default=(datetime.utcnow()))
 
     def __init__(self, *args, **kwargs):
         """Instantiation of base model class
@@ -41,30 +36,22 @@ class BaseModel:
             updated_at: updated date
         """
         if kwargs:
-            self.__set_attributes(kwargs)
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
+                if k != "__class__":
+                    setattr(self, k, v)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
 
-    def __set_attributes(self, attr_dict):
-        """ create instace attributes from a dictionary """
-        if 'id' not in attr_dict:
-            attr_dict['id'] = str(uuid4())
-        if 'created_at' not in attr_dict:
-            attr_dict['created_at'] = datetime.utcnow()
-        elif not isinstance(attr_dict['created_at'], datetime):
-            attr_dict['created_at'] = datetime.strptime(
-                attr_dict['created_at'], "%Y-%m-%d %H:%M:%S.%f")
-        if 'updated_at' not in attr_dict:
-            attr_dict['updated_at'] = datetime.utcnow()
-        elif not isinstance(attr_dict['updated_at'], datetime):
-            attr_dict['updated_at'] = datetime.strptime(
-                attr_dict['updated_at'], "%Y-%m-%d %H:%M:%S.%f")
-
-        if STORAGE_TYPE != 'db':
-            attr_dict.pop('__class__', None)
-        for attr, val in attr_dict.items():
-            setattr(self, attr, val)
 
     def __str__(self):
         """returns a string
