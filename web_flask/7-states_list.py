5f0f805eb756a0d7a5ef097f4bf378ab95b445c7
lucho1503 <lmmc.1503@gmail.com>
2020-04-25 00:04:14 +0000

    fixed

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 """ Create database + start server """
+import models
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def tear_down(exception):
-    """ teardown """
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -22,5 +17,14 @@ def states_list():
     return render_template('7-states_list.html', Table="States", my_dict=dictt)
 
 
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """ teardown """
+    storage.close()
+
+
+
 if __name__ == "__main__":
+    """ run application """
     app.run(host='0.0.0.0', port=5000)
